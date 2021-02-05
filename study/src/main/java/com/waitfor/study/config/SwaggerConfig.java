9f3d2a4e2ff33a396c33935b0bc3a31208c390e8
WaitforzzZ <1204918135@qq.com>
2021-02-05 12:58:26 +0800

    20210205_SpringBoot框架集成Knife4j

--- study/src/main/java/com/waitfor/study/config/SwaggerConfig.java
+++ study/src/main/java/com/waitfor/study/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.waitfor.study.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -9,7 +8,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
  * Swagger 配置类
@@ -18,17 +17,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @since 2021-2-5上午 9:16
  */
 @Configuration
-@EnableSwagger2
+//@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SwaggerConfig {
 
     /**
      * 通过 createRestApi函数来构建一个DocketBean
      * 函数名,可以随意命名,喜欢什么命名就什么命名
      */
-    @Bean
-    public Docket createRestApi() {
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())//调用apiInfo方法,创建一个ApiInfo实例,里面是展示在文档页面信息内容
+                //分组名称
+                .groupName("2.0.7")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.waitfor.study.controller"))
                 .paths(PathSelectors.any())
@@ -37,9 +39,10 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("接口文档")
-                .description("SpringBoot 集成 Swagger")
+                .title("swagger-bootstrap-ui-demo RESTful APIs")
+                .description("# swagger-bootstrap-ui-demo RESTful APIs")
                 .termsOfServiceUrl("https://github.com/WaitforzzZ")
+                .contact("1204918135@qq.com")
                 .version("1.0")
                 .build();
     }
