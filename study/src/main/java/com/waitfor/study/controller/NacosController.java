e99757aa23741f71f9bbd371333e01ce430f0f31
WaitforzzZ <1204918135@qq.com>
2021-09-18 20:00:01 +0800

    feat:动态刷新

--- study/src/main/java/com/waitfor/study/controller/NacosController.java
+++ study/src/main/java/com/waitfor/study/controller/NacosController.java
@@ -2,6 +2,7 @@ package com.waitfor.study.controller;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-9-17下午 9:08
  */
 @RestController
+@RefreshScope
 public class NacosController {
 
     @Value("${isAlive}")
